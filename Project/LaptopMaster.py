4db3236a322c7851f464206390b77f47e75a6c6b
Faaiq Majeed <faaiqmajeed2004@gmail.com>
2025-02-06 23:52:54 -0800

    GUI and 2-way BLE communication working :D

--- Project/LaptopMaster.py
+++ Project/LaptopMaster.py
@@ -1,7 +1,8 @@
 import asyncio
-import tkinter as tk
+import customtkinter as ctk
 from bleak import BleakClient, BleakScanner
 import threading
+from pynput import keyboard
 
 # Define the BLE UUIDs
 laptop_master_service_uuid = "00000000-5EC4-4083-81CD-A10B8D5CF6EC"
@@ -15,11 +16,45 @@ global client, ble_loop
 client = None
 ble_loop = None
 
-def on_button_click():
+# Speed variables
+Forward_Speed = 0
+Back_Speed = 0
+Left_Speed = 0
+Right_Speed = 0
+
+def send_ble_command():
     if client and ble_loop:
-        test_str_bytes = bytearray(str(5), encoding="utf-8")
+        command = f"L:{Left_Speed} R:{Right_Speed} F:{Forward_Speed} B:{Back_Speed}"
+        test_str_bytes = bytearray(command, encoding="utf-8")
         asyncio.run_coroutine_threadsafe(client.write_gatt_char(laptop_master_send_characteristic_uuid, test_str_bytes, response=True), ble_loop)
-        print("Sent 5")
+        print(f"{command}")
+
+def on_press(key):
+    global Forward_Speed, Back_Speed, Left_Speed, Right_Speed
+    try:
+        if key.char == 'w':
+            Forward_Speed += 1
+        elif key.char == 's':
+            Back_Speed += 1
+        elif key.char == 'a':
+            Left_Speed += 1
+        elif key.char == 'd':
+            Right_Speed += 1
+        send_ble_command()
+    except AttributeError:
+        pass
+
+def on_button_click(direction):
+    global Forward_Speed, Back_Speed, Left_Speed, Right_Speed
+    if direction == "Forward":
+        Forward_Speed += 1
+    elif direction == "Back":
+        Back_Speed += 1
+    elif direction == "Left":
+        Left_Speed += 1
+    elif direction == "Right":
+        Right_Speed += 1
+    send_ble_command()
 
 async def run_ble():
     global client, ble_loop
@@ -51,9 +86,24 @@ def start_ble_loop():
 ble_thread = threading.Thread(target=start_ble_loop, daemon=True)
 ble_thread.start()
 
-# Create the Tkinter GUI
-root = tk.Tk()
+# Start listening to keyboard inputs
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+# Create the CustomTkinter GUI
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+root = ctk.CTk()
 root.title("BLE GUI")
-button = tk.Button(root, text="Click Me", command=on_button_click)
-button.pack(pady=20)
+
+button_forward = ctk.CTkButton(root, text="Forward", command=lambda: on_button_click("Forward"))
+button_forward.pack(pady=5)
+button_back = ctk.CTkButton(root, text="Back", command=lambda: on_button_click("Back"))
+button_back.pack(pady=5)
+button_left = ctk.CTkButton(root, text="Left", command=lambda: on_button_click("Left"))
+button_left.pack(pady=5)
+button_right = ctk.CTkButton(root, text="Right", command=lambda: on_button_click("Right"))
+button_right.pack(pady=5)
+
 root.mainloop()
--- Project/robotSlave/robotSlave.ino
+++ Project/robotSlave/robotSlave.ino
@@ -1,7 +1,7 @@
 #include <mbed.h>
 #include <ArduinoBLE.h>
 #include "Arduino_BMI270_BMM150.h"
-#include <math.h> 
+#include <math.h>
 
 using namespace mbed;
 using namespace rtos;
@@ -30,73 +30,97 @@ float Theta_Acc = 0;
 
 //Weighted Values
 float Theta_Final = 0;
-float k = 0.6; //0.6 before
+float k = 0.6;  //0.6 before
 
-float gx_0, gy_0, gz_0, ax_0,ay_0,az_0, x_0, x;
+float gx_0, gy_0, gz_0, ax_0, ay_0, az_0, x_0, x;
+
+//Motor Variables
+int left_Motor_Speed, right_Motor_Speed, forward_Motor_Speed, back_Motor_Speed;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   initializeALL();
-
 }
 
 void loop() {
-
 }
 
-void initializeALL()
-{
-    if (!BLE.begin()) {
+void initializeALL() {
+  if (!BLE.begin()) {
     Serial.println("* Starting Bluetooth® Low Energy module failed!");
-    while (1);
-    }
-    
-    BLE.setLocalName("C5-BLE");
-    BLE.setDeviceName("C5-BLE");
-    BLE.setAdvertisedService(laptopMasterService);
+    while (1)
+      ;
+  }
 
-    laptopMasterService.addCharacteristic(laptopMasterCharacteristic);
-    laptopMasterService.addCharacteristic(laptopMasterReceiveCharacteristic);
-    BLE.addService(laptopMasterService);
+  BLE.setLocalName("C5-BLE");
+  BLE.setDeviceName("C5-BLE");
+  BLE.setAdvertisedService(laptopMasterService);
 
-    BLE.advertise();
+  laptopMasterService.addCharacteristic(laptopMasterCharacteristic);
+  laptopMasterService.addCharacteristic(laptopMasterReceiveCharacteristic);
+  BLE.addService(laptopMasterService);
 
-    connectToMasterLaptop();
+  BLE.advertise();
 
-    if (!IMU.begin()) {
-        Serial.println("Failed to initialize IMU!");
-        while (1);
-    }
+  connectToMasterLaptop();
 
-    readIMUFunction.start(readIMUData);
-    BLEReceiveSendFunction.start(Robot2MasterSendReceive);
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1)
+      ;
+  }
+
+  readIMUFunction.start(readIMUData);
+  BLEReceiveSendFunction.start(Robot2MasterSendReceive);
 }
 
-void connectToMasterLaptop()
-{
+void connectToMasterLaptop() {
   //Continously scan until the master laptop is connected to the arduino robot.
-  while(true)
-  {
+  while (true) {
     laptopMaster = BLE.central();
-    if(laptopMaster) {
-      Serial.println("Connected to MASTER LAPTOP"); 
+    if (laptopMaster) {
+      Serial.println("Connected to MASTER LAPTOP");
       break;
     }
   }
 }
 
-void Robot2MasterSendReceive()
-{
-  while(laptopMaster.connected())
-  {
-    if(laptopMasterReceiveCharacteristic.written())
-    {
+void Robot2MasterSendReceive() {
+  while (laptopMaster.connected()) {
+    if (laptopMasterReceiveCharacteristic.written()) {
+
+      int receiveLength = laptopMasterReceiveCharacteristic.valueLength();
+      byte receiveBuffer[receiveLength];
+      if (laptopMasterReceiveCharacteristic.readValue(receiveBuffer, receiveLength)) {
+
+        String receiveString = String((char*)receiveBuffer);
+
+        left_Motor_Speed = receiveString.substring(2, receiveString.indexOf(' ')).toInt();
+        receiveString = receiveString.substring(receiveString.indexOf(' ') + 3);
+        right_Motor_Speed = receiveString.substring(0, receiveString.indexOf(' ')).toInt();
+        receiveString = receiveString.substring(receiveString.indexOf(' ') + 3);
+        forward_Motor_Speed = receiveString.substring(0, receiveString.indexOf(' ')).toInt();
+        receiveString = receiveString.substring(receiveString.indexOf(' ') + 3);
+        back_Motor_Speed = receiveString.substring(0, receiveString.indexOf(' ')).toInt();
+
+        Serial.print("L: ");
+        Serial.print(left_Motor_Speed);
+        Serial.print(" ");
+        Serial.print("R: ");
+        Serial.print(right_Motor_Speed);
+        Serial.print(" ");
+        Serial.print("F: ");
+        Serial.print(forward_Motor_Speed);
+        Serial.print(" ");
+        Serial.print("B: ");
+        Serial.println(back_Motor_Speed);
+      }
+
       //Receiving data from Laptop Master
-      String Laptop2RobotReceived = laptopMasterReceiveCharacteristic.value();
-      int testReceive = Laptop2RobotReceived.toInt();
-      Serial.println(testReceive);
+      //String Laptop2RobotReceived = laptopMasterReceiveCharacteristic.readValue();
     }
 
     // Read from IMU and Send Data to Laptop Master (convert float to byte array)
@@ -104,19 +128,17 @@ void Robot2MasterSendReceive()
   }
 }
 
-void readIMUData()
-{
-  while(true)
-  {
+void readIMUData() {
+  while (true) {
     if (IMU.gyroscopeAvailable() && IMU.accelerationAvailable()) {
 
       IMU.readGyroscope(gy_0, gx_0, gz_0);
-      IMU.readAcceleration(ay_0,ax_0,az_0);
+      IMU.readAcceleration(ay_0, ax_0, az_0);
 
-      Theta_Acc = atan(ax_0/az_0) * 180/3.14159;
-      Theta_Gyro = gy_0*0.01 + Theta_Final;
+      Theta_Acc = atan(ax_0 / az_0) * 180 / 3.14159;
+      Theta_Gyro = gy_0 * 0.01 + Theta_Final;
 
-      Theta_Final = (Theta_Gyro)*k + Theta_Acc*(1-k);
+      Theta_Final = (Theta_Gyro)*k + Theta_Acc * (1 - k);
     }
   }
 }
